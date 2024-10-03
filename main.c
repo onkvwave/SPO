cd339ac7cc8327b42c9ce6871973d897c25d1480
onkvwave <mrghostpro1002@gmail.com>
2024-10-03 23:23:52 +0500

    fix issue

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
 {
  char first[255], last[255];
  askname(first, last);
- printf("HelloOo, %s %s!\n", first, last);
+ printf("HelloOo, %s %s!\n", first, last); /* for stash (patch) practice */
  return 0;
 }
